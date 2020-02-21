dc6751a05d24079f035c8bb3c54339f187ffa649
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-02-21 07:04:27 +0000

    Update awk reference links

--- basic/awk/awk01.sh
+++ basic/awk/awk01.sh
@@ -3,17 +3,26 @@
 # REFERENCES::
 #  - https://www.setouchino.cloud/blogs/85
 #  - https://it-ojisan.tokyo/awk-if/
+#  - https://www.atmarkit.co.jp/ait/articles/1706/02/news017.html
+#  - https://www.atmarkit.co.jp/ait/articles/1706/08/news015.html
+#  - https://www.atmarkit.co.jp/ait/articles/1706/09/news013.html
+#  - https://www.atmarkit.co.jp/ait/articles/1706/15/news021.html
+#  - https://www.atmarkit.co.jp/ait/articles/1706/16/news019.html
+#  - https://www.atmarkit.co.jp/ait/articles/1706/22/news016.html
+#  - https://www.atmarkit.co.jp/ait/articles/1805/24/news021.html
+#  - https://www.atmarkit.co.jp/ait/articles/1805/25/news035.html
+#  - https://qiita.com/gorilla0513/items/4d1c610f1283bbe46f81
 
 echo '[grep vs awk](01)'
 echo '[grep]'
-cat ./main.sh | grep 'while'
+cat < ./main.sh | grep 'while'
 echo '[awk ]'
-cat ./main.sh | awk '$1=="while" { print $0 }'
+cat < ./main.sh | awk '$1=="while" { print $0 }'
 
 echo '[grep vs awk](02)'
 echo '[grep]'
-cat ./main.sh | grep 'while' | grep -v ':'
+cat < ./main.sh | grep 'while' | grep -v ':'
 echo '[awk ]'
-cat ./main.sh | awk '$1=="while" && $2!=":" { print $0 }'
+cat < ./main.sh | awk '$1=="while" && $2!=":" { print $0 }'
 
 exit 0
--- basic/awk/awk02.sh
+++ basic/awk/awk02.sh
@@ -3,6 +3,15 @@
 # REFERENCES::
 #  - https://www.setouchino.cloud/blogs/85
 #  - https://it-ojisan.tokyo/awk-if/
+#  - https://www.atmarkit.co.jp/ait/articles/1706/02/news017.html
+#  - https://www.atmarkit.co.jp/ait/articles/1706/08/news015.html
+#  - https://www.atmarkit.co.jp/ait/articles/1706/09/news013.html
+#  - https://www.atmarkit.co.jp/ait/articles/1706/15/news021.html
+#  - https://www.atmarkit.co.jp/ait/articles/1706/16/news019.html
+#  - https://www.atmarkit.co.jp/ait/articles/1706/22/news016.html
+#  - https://www.atmarkit.co.jp/ait/articles/1805/24/news021.html
+#  - https://www.atmarkit.co.jp/ait/articles/1805/25/news035.html
+#  - https://qiita.com/gorilla0513/items/4d1c610f1283bbe46f81
 
 echo '[awk -- print column]'
 echo 'a b c d e' | awk '{ print $0 }'
