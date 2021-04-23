a51ce0569067300bcbc7374eff81ecd6b24be639
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-04-23 04:39:00 +0000

    Add grep_remove_emptyline.sh

---
+++ basic/grep/grep_remove_emptyline.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+#
+# REFERENCES:
+#   - https://linuxhint.com/ignore-blank-lines-in-grep/
+#
+
+basedirpath=/tmp/try-linux/grepremoveemptyline
+
+rm -rf "${basedirpath}"
+mkdir -p "${basedirpath}"
+
+## テスト用のファイルを生成
+#
+(
+    cd "${basedirpath}"
+    cat <<EOF >test.txt
+hello
+
+    world
+
+
+  hello2
+
+
+
+
+      world2
+EOF
+)
+
+## grep コマンドで空行を除去
+#
+fpath="${basedirpath}/test.txt"
+
+## 元データを表示
+#
+echo '------------------- Original ----------------------------'
+cat "${fpath}"
+
+# (1) [:space:] を使う
+echo '------------------- use [:space:] ----------------------------'
+grep -E -v '^[[:space:]]*$' "${fpath}"
+
+# (2) '\S' を使う
+echo '------------------- use \S ----------------------------'
+grep '\S' "${fpath}"
+
+# (3) '^$' を使う
+echo '------------------- use ^$ ----------------------------'
+grep -E -v '^$' "${fpath}"
